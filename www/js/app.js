47404027a00be0e7acbf45861b8e4a819ca919b0
rachelil <rachelil@ironsr.com>
2016-11-03 19:34:52 +0200

    adding events form

--- www/js/app.js
+++ www/js/app.js
@@ -7,92 +7,103 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-
-  $ionicConfigProvider.tabs.position('bottom');
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html'
-  })
-
-
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.my-events', {
-    url: '/my-events',
-    views: {
-      'tab-my-events': {
-        templateUrl: 'templates/tab-events.html',
-        controller: 'MyEventsController'
-      }
-    }
-  })
-
-    .state('tab.my-settings', {
-      url: '/my-settings',
-      views: {
-        'tab-my-settings': {
-          templateUrl: 'templates/tab-my-settings.html',
-          // controller: 'MySettingsController'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
-    .state('tab.admin-settings', {
-      url: '/admin-settings',
-      views: {
-        'tab-admin-settings': {
-          templateUrl: 'templates/admin-settings.html'
-        }
-      }
-    })
-    .state('tab.admin-users', {
-      url: '/admin-users',
-      views: {
-        'tab-admin-settings': {
-          templateUrl: 'templates/admin-users.html'
-        }
-      }
-    })
-    .state('tab.admin-events', {
-      url: '/admin-events',
-      views: {
-        'tab-admin-settings': {
-          templateUrl: 'templates/admin-events.html'
-        }
-      }
+
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+        $ionicConfigProvider.tabs.position('bottom');
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html'
+            })
+
+
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.my-events', {
+                url: '/my-events',
+                views: {
+                    'tab-my-events': {
+                        templateUrl: 'templates/tab-events.html',
+                        controller: 'MyEventsController'
+                    }
+                }
+            })
+
+            .state('tab.my-settings', {
+                url: '/my-settings',
+                views: {
+                    'tab-my-settings': {
+                        templateUrl: 'templates/tab-my-settings.html',
+                        // controller: 'MySettingsController'
+                    }
+                }
+            })
+            .state('tab.admin-settings', {
+                url: '/admin-settings',
+                views: {
+                    'tab-admin-settings': {
+                        templateUrl: 'templates/admin-settings.html'
+                    }
+                }
+            })
+            .state('tab.admin-users', {
+                url: '/admin-users',
+                views: {
+                    'tab-admin-settings': {
+                        templateUrl: 'templates/admin-users.html'
+                    }
+                }
+            })
+            .state('tab.admin-events', {
+                url: '/admin-events',
+                views: {
+                    'tab-admin-settings': {
+                        templateUrl: 'templates/admin-events.html'
+                    }
+                }
+            })
+            .state('tab.edit-event', {
+                url: '/edit-event?id',
+                params: {
+                    id: null
+                },
+                views: {
+                    'tab-admin-settings': {
+                        templateUrl: 'templates/edit-event.html'
+                    }
+                }
+            })
+
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/my-settings');
+
     });
-
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/my-settings');
-
-});
