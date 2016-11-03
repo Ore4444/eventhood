e5c07e49783cf4953d9e7ff43df07e4fe427f8ef
Ore Landau <ore.landau@ironsrc.com>
2016-11-03 13:14:25 +0200

    login

--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,41 @@
+
+
 angular.module('starter.services', [])
+
+.factory('firebaseInit', ['$firebaseObject', function($firebaseObject) {
+
+
+  var fireBase;
+
+  function createInstance (){
+     fireBase = $firebaseObject;
+
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: "AIzaSyCbFIwQ3Z9ovnrZgsqe5unQnikVV92DAhQ",
+      authDomain: "hackathonproj-12729.firebaseapp.com",
+      databaseURL: "https://hackathonproj-12729.firebaseio.com",
+      storageBucket: "hackathonproj-12729.appspot.com",
+      messagingSenderId: "218970006552"
+    });
+
+    return this.fireBase;
+  }
+
+  function getInstance() {
+    if (!fireBase) {
+      createInstance();
+    }
+    return fireBase;
+  }
+
+
+  return {
+    getInstance: getInstance,
+  }
+
+}])
+
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
